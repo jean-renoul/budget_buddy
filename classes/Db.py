e2804d127339710a72779991e6c6867d3ac415ef
jean-renoul <jean.renoul@laplateforme.io>
2024-03-25 12:15:42 +0100

    added class Db and started the password verification process

---
+++ classes/Db.py
@@ -0,0 +1,36 @@
+import mysql.connector
+
+class Db:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+
+    def connect(self):
+        self.connection = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+        self.cursor = self.connection.cursor()
+
+    def disconnect(self):
+        self.connection.close()
+
+    def executeQuery(self, query, params=None):
+        self.connect()
+        self.cursor.execute(query, params or ())
+        self.connection.commit()
+        self.disconnect()
+
+    def fetch(self, query, params=None):
+        self.connect()
+        self.cursor.execute(query, params or ())
+        result = self.cursor.fetchall()
+        self.disconnect()
+        return result
+    
+if __name__ == "__main__":
+    db = Db("82.165.185.52", "budget-buddy", "database-budget-buddy", "budget-buddy")
