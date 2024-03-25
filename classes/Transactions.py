0be0e94ebc606b2a743580c529e60001c3e17223
jean-renoul <jean.renoul@laplateforme.io>
2024-03-25 11:07:06 +0100

    creation Users and Transactions classes

---
+++ classes/Transactions.py
@@ -0,0 +1,8 @@
+class Transactions:
+    def __init__(self, name, date, category, description, amount, type):
+        self.name = name
+        self.date = date
+        self.category = category
+        self.description = description
+        self.amount = amount
+        self.type = type
---
+++ classes/Users.py
@@ -0,0 +1,8 @@
+class Users:
+    def __init__(self, last_name, first_name, email, password, balance=1000.00):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.email = email
+        self.password = password
+        self.balance = balance
+        
