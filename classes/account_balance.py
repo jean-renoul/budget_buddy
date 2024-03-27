2ee0b5c0a2eb358516afc0017e722223defe8d95
mounir-merzoud <mounir.merzoud@laplateforme.io>
2024-03-27 01:16:09 +0100

    finalization of the file account_balance.py

---
+++ classes/account_balance.py
@@ -0,0 +1,50 @@
+import mysql.connector
+
+class Db:
+    def __init__(self, host, user, password, database):
+        self.connection = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+        self.cursor = self.connection.cursor()
+
+    def fetch(self, query, params=None):
+        self.cursor.execute(query, params or ())
+        return self.cursor.fetchall()
+
+
+class AccountBalance:
+    def __init__(self, name):
+        self.name = name
+        self.db = Db("82.165.185.52", "budget-buddy", "database-budget-buddy", "jean-renoul_budget-buddy")
+
+    def get_transactions(self):
+        query = "SELECT user_id, amount, type FROM transactions WHERE name = %s ORDER BY transaction_id"
+        params = (self.name,)
+        transactions = self.db.fetch(query, params)
+        return transactions
+
+    def calculate_balance(self):
+        transactions = self.get_transactions()
+        balance = 1000  # Solde initial
+        prev_user_id = None
+        for user_id, amount, type in transactions:
+            if prev_user_id is None or prev_user_id != user_id:
+                # Nouvel ID, imprimer le solde actuel
+                if prev_user_id is not None:
+                    print(f"Solde actuel pour l'utilisateur avec l'ID {prev_user_id}: {balance}")
+                balance = 1000  # Réinitialiser le solde pour le nouvel ID
+            if type == "revenu":
+                balance += amount
+            elif type == "dépense":
+                balance -= amount
+            prev_user_id = user_id
+        # Dernier solde pour le dernier utilisateur
+        if prev_user_id is not None:
+            print(f"Solde actuel pour l'utilisateur avec l'ID {prev_user_id}: {balance}")
+
+if __name__ == "__main__":
+    account_balance = AccountBalance("Renoul Jean")  # Remplacez par le nom de l'utilisateur souhaité
+    account_balance.calculate_balance()
